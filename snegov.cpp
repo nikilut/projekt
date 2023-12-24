df439b47ec9f42255f27a7992c1aeabba185c071
ITSTEP\st <st>
2023-12-24 19:02:24 +0300

    sneg

---
+++ snegov.cpp
@@ -0,0 +1,25 @@
+#include <ctime>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    cout << "              &                          " << endl;
+    cout << "              **                         " << endl;
+    cout << "               ***                       " << endl;
+    cout << "               *****                     " << endl;
+    cout << "              *******                    " << endl;
+    cout << "             **.***.**                   " << endl;
+    cout << "             *********                   " << endl;
+    cout << "         \     *---*     /               " << endl;
+    cout << "          \   *******   /                " << endl;
+    cout << "           \*****o*****/                 " << endl;
+    cout << "            ***********                  " << endl;
+    cout << "              ***o***                    " << endl;
+    cout << "              *******                    " << endl;
+    cout << "            *****o*****                  " << endl;
+    cout << "            ***********                  " << endl;
+    cout << "              *******                    " << endl;
+}                          
